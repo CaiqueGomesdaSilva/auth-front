3ba0de58772d53e0053878d5a22766a63bfd20c0
CaiqueGomesdaSilva <@caique.gsilva@sptech.school>
2023-07-18 15:13:26 -0300

    funcao logout criada + tratamento de erros

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, catchError, map, throwError } from 'rxjs';
 
 @Injectable({
@@ -9,18 +10,28 @@ export class AuthService {
 
   private url: string = 'http://localhost:3000';
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router
   ) { }
 
   public sign(payLoad: {email: string, password: string}): Observable<any>{
-    return this.http.post(`${this.url}/sign`, payLoad).pipe(
+    return this.http.post<{token: string}>(`${this.url}/sign`, payLoad).pipe(
       map(
-        (data) => {
-          return console.log(data)
+        (res) => {
+          localStorage.removeItem('access_token');
+          localStorage.setItem('access_token', res.token);
+          return this.router.navigate(['admin']);
         }),
-        catchError((err) => {
-          return throwError(() => err.error.message);
+        catchError((e) => {
+          if(e.error.message) return throwError(() => e.error.message);
+          
+          return throwError(() => "No momento não estamos conseguindo validar estes dados, tente novamente mais tarde.");
         })
     )
+  }
+
+  public logout(){
+    localStorage.removeItem('access_token');
+    return this.router.navigate([''])
   }
 }
